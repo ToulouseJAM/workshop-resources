76dce73822b952a680954b0254e75cf411dfdfec
Michaël Pailloncy <michael.pailloncy@gmail.com>
2017-03-09 22:34:52 +0100

    Add workshop3 correction

---
+++ workshop3.groovy
@@ -0,0 +1,90 @@
+#!/usr/bin/env groovy
+
+pipeline {
+
+    agent none
+
+    stages {
+
+        stage("build & unit tests") {
+            agent { label "build "}
+            steps {
+                withMaven(maven: "M3") {
+                    sh "mvn clean install"
+                }
+            }
+            post {
+                success {
+                    stash includes: 'target/*.jar', name: 'binary'
+                }
+            }
+        }
+
+        stage("static-analysis") {
+            agent { label "build "}
+            steps {
+                withMaven(maven: "M3") {
+                    withSonarQubeEnv("sonarqube") {
+                        sh "mvn sonar:sonar"
+                    }
+                }
+            }
+        }
+
+        stage("test") {
+            agent any
+            steps {
+                parallel (
+                    "firefox" : {
+                        node {
+                            sleep 2
+                        }
+                    },
+                    "chrome" : {
+                        node {
+                            sleep 2
+                        }
+                    },
+                    "edge" : {
+                        node {
+                            sleep 2
+                        }
+                    }
+                )
+            }
+        }
+
+        stage("manual-approval") {
+            when {
+                branch "master"
+            }
+            steps {
+                input "sur ?"
+            }
+        }
+
+        stage("staging") {
+            agent any
+            when {
+                branch "master"
+            }
+            steps {
+                deleteDir()
+                unstash "binary"
+                deploy("root", "STAGING_IP", "target/*.jar")
+            }
+        }
+
+        stage("prod") {
+            agent any
+            when {
+                branch "master"
+            }
+            steps {
+                deleteDir()
+                unstash "binary"
+                deploy("root", "PROD_IP", "target/*.jar")
+            }
+        }
+    }
+}

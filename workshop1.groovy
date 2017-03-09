ff322984aaf196627b4e0909071c6e3f79620a05
Michaël Pailloncy <michael.pailloncy@gmail.com>
2017-03-09 11:19:11 +0100

    Add workshop#1 correction

---
+++ workshop1.groovy
@@ -0,0 +1,53 @@
+#!/usr/bin/env groovy
+
+stage("build & unit tests") {
+    node("build") {
+        sleep 10
+    }
+}
+
+stage("static-analysis") {
+    node("build") {
+        sleep 10
+    }
+}
+
+stage("acceptance-tests") {
+
+    def tests = [
+            "firefox" : {
+                node {
+                    sleep 10
+                }
+            },
+            "chrome" : {
+                node {
+                    sleep 10
+                }
+            },
+            "edge" : {
+                node {
+                    sleep 10
+                }
+            }
+    ]
+
+    parallel tests
+
+}
+
+stage("staging") {
+    node {
+        sleep 10
+    }
+}
+
+stage("manual-approval") {
+    input "Deploiement en production ?"
+}
+
+stage("deploy") {
+    node {
+        sleep 10
+    }
+}
